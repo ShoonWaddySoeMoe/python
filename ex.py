abd0992393ada54efd7c54f2d0b6d09c6db11053
ShoonWaddySoeMpe <shoonwaddy777@gmail.com>
2018-06-09 18:20:14 +0630

    change

---
+++ ex.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+print("Hello Again")
+print("I like typing this.")
+print("This is funny.")
+print('Yay! printing.')
+print(f"I'd much rather you 'not'.")
+print('I "said " do not touch this.')
+
