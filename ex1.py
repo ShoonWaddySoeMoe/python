abcb3b836ac691255c127871225aaabfac6a05da
ShoonWaddySoeMpe <shoonwaddy777@gmail.com>
2018-06-10 17:47:52 +0630

    change

--- ex1.py
+++ ex1.py
@@ -1,6 +1,8 @@
- print("How tall are you?", end=' ')
-4 height = input()
-5 print("How much do you weigh?",end=' ')
-6 weight =input()
-7
-8 print(f "So,you're {age} old,{height} tall and {weight} heavy.")
+age = input("How old are you?" ,end=' ')
+age = input ()
+print("How tall are you?", end=' ')
+ height = input()
+ print("How much do you weigh?",end=' ')
+ weight =input()
+
+ print(f "So,you're {age} old,{height} tall and {weight} heavy.")
