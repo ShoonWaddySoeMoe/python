83c2bd44ae4b40e8a6a6e6feba076c2094341cbe
ShoonWaddySoeMoe <shoonwaddy777@gmail.com>
2018-08-23 17:35:42 +0630

    This is my ex16 for python exercise

--- ex16.py
+++ ex16.py
@@ -2,7 +2,7 @@ from sys import argv
 
 script, filename = argv
 
-print(f"We're going to erase {filename}.")
+print(f"We're gone to erase {filename}.")
 print("If you don't want that, hit CTRL-C (^C).")
 print("If you do want that, hit RETURN.")
 
