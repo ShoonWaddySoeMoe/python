b066413a95a6164942396953f9779375b8108730
ShoonWaddySoeMoe <shoonwaddy777@gmail.com>
2018-08-23 17:51:01 +0630

    This is my ex25 for python exercise

--- ex25.py
+++ ex25.py
@@ -1,5 +1,5 @@
 def break_words(stuff):
-     """This function will break up words for us."""
+     """The function will break up words for us."""
      words = stuff.split(' ')
      return words
 
