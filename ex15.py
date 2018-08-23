934ad647e5d481e3b64f3f5b4d017270fbeb26a6
ShoonWaddySoeMoe <shoonwaddy777@gmail.com>
2018-08-23 17:30:52 +0630

    This is my ex15 for python exercise

--- ex15.py
+++ ex15.py
@@ -2,7 +2,7 @@ from sys import argv
 #read the WYSS section for how to run this 
 script,first, second, third = argv
 
-print("The script is called:", script)
+print("The script called:", script)
 print("Your first variable is:", first)
 print("Your second variale is:", second)
 print("Your third variale is:", third)
