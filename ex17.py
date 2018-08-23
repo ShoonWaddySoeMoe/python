59c8d94ce012a1683e186baaecbc5ee116138561
ShoonWaddySoeMoe <shoonwaddy777@gmail.com>
2018-08-23 17:36:48 +0630

    This is my ex17 for python exercise

--- ex17.py
+++ ex17.py
@@ -3,7 +3,7 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print(f"Copying from {from_file} to {to_file}")
+print(f"Copy from {from_file} to {to_file}")
 
 # we could do these two on one line, how?
 in_file = open(from_file)
