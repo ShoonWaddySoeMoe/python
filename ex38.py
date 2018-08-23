45dcdd468583f2d6a828293932b230c6c3ca4773
ShoonWaddySoeMoe <shoonwaddy777@gmail.com>
2018-08-23 18:05:42 +0630

    This is my ex38 for python exercise

--- ex38.py
+++ ex38.py
@@ -1,6 +1,6 @@
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
-print("Wait there are not 10 things in that list. Let's fix that.")
+print("Wait there are not 10 thing in that list. Let's fix that.")
 
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee",
